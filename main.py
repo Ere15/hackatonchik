91f3b15d4a4f3ad53180571a0e930ccccaa4351c
4116VyachelavPyatetsky <115720162+4116VyachelavPyatetsky@users.noreply.github.com>
2024-03-16 08:29:55 +0300

    api

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from neoapp.app.api.endpoints.api import router as api_router 
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 
 # Создание экземпляра приложения FastAPI
 app = FastAPI()
@@ -7,6 +9,25 @@ app = FastAPI()
 # Подключение маршрутизатора с эндпоинтами к приложению
 app.include_router(api_router)
 
+app.mount("/static", StaticFiles(directory="neoapp/front/static"), name="static")
+
+@app.get("/", response_class=HTMLResponse)
+async def get_html():
+    with open(r"neoapp\front\teamplates\Login.html", "r",encoding="utf-8", errors="ignore") as file:
+        html_content = file.read()
+    return html_content
+
+@app.get("/employee", response_class=HTMLResponse)
+async def get_page1():
+    with open(r"neoapp\front\teamplates\employee.html", "r",encoding="utf-8", errors="ignore") as file:
+        html_content = file.read()
+    return html_content
+
+@app.get("/request", response_class=HTMLResponse)
+async def get_page2():
+    with open(r"neoapp\\front\\teamplates\\request_details.html", "r",encoding="utf-8", errors="ignore") as file:
+        html_content = file.read()
+    return html_content
 
 
 # Запуск сервера
